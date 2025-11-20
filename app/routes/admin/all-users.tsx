1e19dbd0c984ba3c219cbd555e59c94ca746d26b
Angelo Mubita <angelomubita@gmail.com>
2025-11-20 15:39:59 +0200

    Implement Routing, Dashboard Layout, Auth Functionality

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,14 @@
+import { Header } from "components";
+
+const AllUsers = () => {
+  return (
+    <main className="admin wrapper">
+      <Header
+        title="Trips page"
+        description="Tap into our Touristo community real-time"
+      />
+    </main>
+  );
+};
+
+export default AllUsers;
---
+++ components/NavItems.tsx
@@ -0,0 +1,75 @@
+import { Link, NavLink } from "react-router";
+import { sidebarItems } from "~/constants";
+import { cn } from "~/lib/utils";
+
+const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
+  const user = {
+    name: "Angelo",
+    email: "angelomubita@gmail.com",
+    imageUrl: "/assets/images/david.webp",
+  };
+
+  return (
+    <section className="nav-items">
+      <Link to="/" className="link-logo">
+        <img
+          src="/assets/icons/logo.svg"
+          alt="logo-full"
+          className="size-[30px]"
+        />
+        <h1>Touristo</h1>
+      </Link>
+
+      <div className="container">
+        <nav>
+          {sidebarItems.map(({ id, href, icon, label }) => (
+            <NavLink to={href} key={id}>
+              {({ isActive }: { isActive: boolean }) => (
+                <div
+                  className={cn("group nav-item", {
+                    "bg-primary-100 text-white!": isActive,
+                  })}
+                  onClick={handleClick}
+                >
+                  <img
+                    src={icon}
+                    alt={label}
+                    className={`group-hover:brightness-0 size-0 group-hover:invert ${isActive ? "brightness-0" : "text-dark-200"}`}
+                  />
+                  {label}
+                </div>
+              )}
+            </NavLink>
+          ))}
+        </nav>
+
+        <footer className="nav-footer">
+          <img
+            src={user?.imageUrl || "/assets/images/david.webp"}
+            alt={user?.imageUrl || "Angelo"}
+          />
+
+          <article>
+            <h2>{user?.name}</h2>
+            <h2>{user?.email}</h2>
+          </article>
+
+          <button
+            onClick={() => {
+              console.log("logout");
+            }}
+            className="cursor-pointer"
+          >
+            <img
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              className="size-6"
+            />
+          </button>
+        </footer>
+      </div>
+    </section>
+  );
+};
+
+export default NavItems;
